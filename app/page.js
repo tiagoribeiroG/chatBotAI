f512ea655c01b61a9546413ac186011426ce4da3
tiagoribeiroG <tiagoribeiro1246@gmail.com>
2024-04-01 21:24:13 -0300

    feat api

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ const openai = new OpenAI({
 export default function Home() {
   const [userInput, setUserInput] = useState('');
   const [chatHistory, setChatHistory] = useState([]);
-  const [isLoading, seIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleUserInput = async () => {
     setIsLoading(true);
@@ -29,14 +29,14 @@ export default function Home() {
       {role: 'assistant', content: chatCompletion.choices[0].message.content},
     ]);
 
-    setUserImput('');
+    setUserInput('');
     setIsLoading(false);
 
   }
 
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
-      <div className="w-full max-w-screen-md mg-white p-4rounded-lg shad-md">
+    <div className="bg-gray-300 min-h-screen flex flex-col justify-center items-center">
+      <div className="w-full max-w-screen-md mg-white p-4 rounded-lg shadow-md">
         <div className="mb-4">
           <div className="text-4xl- font-bold text-blue-800 mb-2">
             Chatbot Assistant
@@ -52,11 +52,40 @@ export default function Home() {
             key={index}
             className={`${
               message.role === 'user' ? 'text-left' : 'text-right'
-            }`}
+            } mb-2`}
             >
-              
+              <div className={`rounded-full p-2 max-w-md mx-4 inline-block ${message.role === 'user' ? 
+              'bg-blue-300 text-blue-800' : 'bg-green-300 text-green-800'}`}>
+                {message.role === 'user' ? 'H' : 'A'}
+              </div> 
+              <div className={`max-w-md mx-4 my-2 inline-block ${message.role === 'user' ? 'bg-blue-100 text-blue-800' : 'bg-green-100 text-green-800'} 
+              p-2rounded-md`}>
+                {message.content}
+              </div>
             </div>
           ))}
+        </div>
+
+        <div className="flex">
+          <input 
+          type="text"
+          placeholder="Ask me something.."
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+          className="flex-1 p-2 rounded-l-lg"
+          />
+          {isLoading ? (
+            <div className="bg-blue-500 text-white p-2 rounded-r-lg animate-pulse">
+              Loading...
+            </div>
+          ) : (
+            <button onClick={handleUserInput}  
+            className="bg-blue-500 text-white p-2 rounded-r-lg hover:bg-blue-600"
+            >
+              Ask
+            </button>
+          )}
+
         </div>
       </div>
     </div>
